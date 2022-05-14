8effdca24dfaaf34507d6989f184b66155c333c1
Luian Ortiz <luiann.ortiz@gmail.com>
2022-05-14 18:57:55 -0500

    Add animal's sedd and server.js

--- prisma/seed_animal.js
+++ prisma/seed_animal.js
@@ -6,45 +6,45 @@ const prisma = new PrismaClient();
     const animal = await prisma.animal.createMany({
       data: [
         {   
-            name: "Vaquita Marina", scientific_name: "", 
-            description: "Se localiza en la reserva de la biosfera de las aguas mexicanas del Golfo de California, es uno de los cetáceos más pequeños del mundo (150 cm) con un peso promedio de 50 kg.", 
-            url_image: "https://energiahoy.com/wp-content/uploads/2021/06/Vaquita-marina-1024x572.jpeg",
-            url_info: "", 
-            habitadId: 2, riskstatusId: 5
+            name: "Lobo fino de las Galápagos", scientific_name: "Arctocephalus galapagoensis", 
+            description: "Foca de Galápagos, Lobo de dos pelos de Galápagos, Lobo fino de Galápagos, Lobo fino de las Galápagos, Oso Marino de las Galápagos", 
+            url_image: "https://inaturalist-open-data.s3.amazonaws.com/photos/25677704/large.jpg",
+            url_info: "https://enciclovida.mx/especies/33935-arctocephalus-galapagoensis", 
+            habitadId: 2, riskstatusId: 3
         },
         {   
-            name: "Totoaba", scientific_name: "", 
-            description: "", 
-            url_image: "https://energiahoy.com/wp-content/uploads/2021/06/totoaba.jpeg",
-            url_info: "Se localiza en el norte del Golfo de California de México. Este pez enfrenta desde hace décadas una dura caza furtiva porque su buche es muy codiciado en China. Su pesca supone el sustento principal de habitantes de pueblos como San Felipe o Puerto Peñasco Sonora.", 
-            habitadId: 2, riskstatusId: 1
+            name: "Tiburón coralino", scientific_name: "Carcharhinus perezii", 
+            description: "Tiburón de arrecife", 
+            url_image: "https://inaturalist-open-data.s3.amazonaws.com/photos/137461/large.jpg?1545363116",
+            url_info: "https://europe.oceana.org/es/eu/que-hacemos/fauna-y-flora-marina/tiburones/especies-en-peligro/tiburon-coralino", 
+            habitadId: 2, riskstatusId: 3
             },
         {   
-            name: "Tortuga carey", scientific_name: "", 
-            description: "La población ha disminuido hasta un 80% en los últimos años debido a la pesca desmesurada en China y Japón. Sin embargo, sobrevive en las zonas tropicales.", 
-            url_image: "https://energiahoy.com/wp-content/uploads/2021/06/Tortuga_carey.jpeg", 
-            url_info: "", 
-            habitadId: 1, riskstatusId: 2
+            name: "Espada de Monterrey", scientific_name: "Xiphophorus couchianus", 
+            description: "Una descripcion jeje", 
+            url_image: "https://inaturalist-open-data.s3.amazonaws.com/photos/15494451/large.jpeg", 
+            url_info: "https://colombia.inaturalist.org/taxa/115170-Xiphophorus-couchianus", 
+            habitadId: 1, riskstatusId: 1
         },
         {   
-            name: "Ballena azul", scientific_name: "", 
-            description: "Con un tamaño que va de 24 a 30 metros y un peso de alrededor de 173 toneladas, la ballena azul se encuentra en la cima de la cadena alimenticia, con un papel importante para mantener el balance de los ecosistemas marinos. ", 
-            url_image: "https://energiahoy.com/wp-content/uploads/2021/06/Ballena-azul.jpeg", 
-            url_info: "", 
-            habitadId: 2, riskstatusId: 2
+            name: "Tiburón zorro común", scientific_name: "Alopias vulpinus", 
+            description: "zotador, Tiburón ratón, Tiburón zorro común", 
+            url_image: "https://static.inaturalist.org/photos/61205816/large.jpg", 
+            url_info: "https://www.naturalista.mx/taxa/93696", 
+            habitadId: 2, riskstatusId: 4
         },
         {   
-            name: "Tortuga Lora", scientific_name: "", 
+            name: "Tortuga Lora", scientific_name: "Lepidochelys kempii", 
             description: "También conocida como tortuga cotorra, tortuga golfina o tortuga bastarda, es uno de los animales en mayor peligro de extinción", 
             url_image: "https://energiahoy.com/wp-content/uploads/2021/06/Tortuga-lora.jpeg", 
-            url_info: "", 
-            habitadId: 3, riskstatusId: 3
+            url_info: "http://www.iacseaturtle.org/docs/tortugas/lkempi.pdf", 
+            habitadId: 3, riskstatusId: 2
         },
         {  
-             name: "León Marino", scientific_name: "", 
+             name: "León Marino", scientific_name: "Otariinae", 
             description: "Se localiza en las frías aguas costeras del Pacífico norte. Es el miembro más grande de la familia Otariid y la cuarta especie de foca más larga del mundo. ", 
             url_image: "https://energiahoy.com/wp-content/uploads/2021/06/Leo%CC%81n-marino-de-Steller-1024x679.png", 
-            url_info: "", 
+            url_info: "https://www.sdnhm.org/oceanoasis/fieldguide/zalo-cal-sp.html", 
             habitadId: 2, riskstatusId: 4
         },
         
